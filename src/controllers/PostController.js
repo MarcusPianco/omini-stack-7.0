b5623043d0a1ac59270048f3bd630c4b9c5c1202
marcus pianco <marcus.ufal@gmail.com>
2019-06-12 21:53:05 -0300

    Criada a funcionalidade para add Post e dar like

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,8 +1,33 @@
 const Post = require("../models/Post");
+const sharp = require("sharp");
+const path = require("path");
+const fs = require("fs");
 
 module.exports = {
-  async index(req, res) {},
+  async index(req, res) {
+    const posts = await Post.find().sort("-createdAt");
+
+    return res.json(posts);
+  },
   async store(req, res) {
-    return res.json({ ok: true });
+    const { author, place, description, hashtags } = req.body;
+    const { filename: image } = req.file;
+
+    await sharp(req.file.path)
+      .resize(500)
+      .jpeg()
+      .toFile(path.resolve(req.file.destination, "resized", image));
+
+    fs.unlinkSync(req.file.path);
+
+    const post = await Post.create({
+      author,
+      place,
+      description,
+      hashtags,
+      image
+    });
+
+    return res.json(post);
   }
 };
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const PostController = require("./controllers/PostController");
-
+const multer = require("multer");
+const uploadConfig = require("./config/upload");
+const LikeController = require("./controllers/LikeController");
 const routes = new express.Router();
+const upload = multer(uploadConfig);
 
-routes.post("/posts", PostController.store);
+routes.post("/posts", upload.single("image"), PostController.store);
+routes.post("/posts/:id/like", LikeController.store);
+
+routes.get("/posts", PostController.index);
 
 // #midlleware intercepta a rota
 routes.get("/", (req, res) => {
